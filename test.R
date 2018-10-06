783acd073db1cd9f8efc8d84b657f9e7813eda8d
Dallin Webb <webbdallin@gmail.com>
2018-10-06 15:41:19 -0600

    update

--- test.R
+++ test.R
@@ -1,10 +1,10 @@
-library(tidyverse)
 
 
-
-wds <- read_lines("Lesson24.Rmd")
-
-wds_new <- wds %>% 
+wiki_to_md <- function(wiki_file){
+  library(tidyverse)
+  library(glue)
+  x <- read_lines(wiki_file)
+  new <- x %>% 
   str_replace_all("^={5}", "##### ") %>% 
   str_remove_all( "={5}$")          %>% 
   str_replace_all("^={4}", "#### ") %>% 
@@ -15,11 +15,18 @@ wds_new <- wds %>%
   str_remove_all( "={2}$")          %>% 
   str_replace_all("'{3}", "**")     %>% 
   str_replace_all("'{2}", "*")      %>% 
-  str_replace_all("^\\[\\[(File):", '<img src="./Images/') %>% 
-  str_replace_all("\\|(link).*\\]\\]$", '">') %>% 
-  str_replace_all("\\<div style.*", '<a href="javascript:showhide("Q2")"><span style="font-size:8pt;">Show/Hide Solution</span></a>') %>% 
-  str_replace_all('\\<div class="mw\\-.*', '<div id="Q2" style="display:none;">') %>% 
-  str_replace_all('showhide\\("Q2"',"showhide('Q2'")
+  str_replace_all("^:{2}", "* ")      %>% 
+  str_remove_all( "^:{1}")           %>% 
+  str_replace_all("^\\[\\[(File):", '<img src="./Images/') %>%
+  str_replace_all("\\|(link).*\\]\\]$", '">') %>%
+  str_replace_all("\\<div style.*", glue('<a href="javascript:showhide(', "'Q2')",'"><span style="font-size:8pt;">Show/Hide Solution</span></a>')) %>%
+  str_replace_all('\\<div class="mw\\-.*', '<div id="Q2" style="display:none;">')
+  new_file_name <- paste0("converted", wiki_file)
+  write_lines(new, new_file_name)
+  remove(x,new)
+}
 
-
-write_lines(wds_new, "Lesson24_conv.Rmd")
+# files <- list.files(pattern = "Lesson\\d{1,2}(.Rmd)")
+# files_list <- map(files, read_lines)
+# converted_files <- map(files_list[2:22], wiki_to_md)
+# map(2:22, ~ write_lines(converted_files, paste0("Lesson", ., "_converted.Rmd")))
