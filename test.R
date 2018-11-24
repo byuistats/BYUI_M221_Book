91ba08ba352ae2753fd0de393cd81f34834e330f
Dallin Webb <webbdallin@gmail.com>
2018-11-24 08:52:51 -0700

    Lesson 18, 19, 20, & 21

--- test.R
+++ test.R
@@ -5,8 +5,8 @@ wiki_to_md <- function(wiki_file){
   require(glue)
   x <- read_lines(wiki_file)
   new <- x %>% 
+# Step 1: Fix Headers ----------------------------------------------------------
   str_replace_all("^={5}", "##### ") %>% 
-    
   str_remove_all( "={5}$")          %>% 
   str_replace_all("^={4}", "#### ") %>% 
   str_remove_all( "={4}$")          %>% 
@@ -14,12 +14,14 @@ wiki_to_md <- function(wiki_file){
   str_remove_all( "={3}$")          %>% 
   str_replace_all("^={2}", "## ")   %>% 
   str_remove_all( "={2}$")          %>% 
-  str_replace_all("^:{1}\\*", "- ") %>% 
-  #str_replace_all("^\\*{1}", "- ")  %>% 
-  str_replace_all("^:{2}\\*", "  + ") %>% 
+# Step 2: Fix bullet points ----------------------------------------------------
+  str_replace_all("^\\:\\*\\s", "- ") %>% 
+  str_replace_all("^\\:\\*{2}\\s", "  + ") %>% 
+# Step 3: Fix Bold & Italics ---------------------------------------------------
   str_replace_all("'{3}", "**")     %>% 
   str_replace_all("'{2}", "*")      %>% 
-  str_replace_all("^:{2}", "* ")    %>% 
+  str_replace_all("^:{2}\*", "- ")  %>% 
+  str_replace_all("^:{2}", "- ")    %>% 
   str_remove_all("^:{1}")           %>% 
   str_replace_all("^\\[\\[(File):", '<img src="./Images/') %>%
   str_replace_all("\\|(link).*\\]\\]", '">') %>%
@@ -29,13 +31,15 @@ wiki_to_md <- function(wiki_file){
   str_replace_all('\\<div class="mw\\-.*', '<div id="Q2" style="display:none;">') %>% 
   str_replace_all("\\<\\w*\\>\\$\\<\\/\\w*\\>", '?') %>% 
   #str_replace_all("\\s\\?", "\\$") %>% 
-  str_replace("toc\\_float:\\strue$", "toc_float: false")
-  
+  str_replace("toc\\_float:\\strue$", "toc_float: false") %>% 
+  str_replace_all("\\<(span class)\\=(Link)\\>\\[\\[(Data)\\|(\\w*)\\]\\]\\<\\/(span)\\>",
+                  "[\\4](./Data/\\4.xlsx)")
   new_file_name <- paste0("converted", wiki_file)
   write_lines(new, new_file_name)
-  remove(x, new, new_file_name)
   
 }
+
+
 
 # files <- list.files(pattern = "Lesson\\d{1,2}(.Rmd)")
 # files_list <- map(files, read_lines)
